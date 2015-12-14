f9663d7b0bd7778fa36306270262636ca0d24b43
Bucky <buckyroberts@gmail.com>
2015-12-13 22:29:38 -0500

    Added source code.

---
+++ 01_basicApp/main.py
@@ -0,0 +1,15 @@
+from flask import Flask
+
+# Pass in name to determine root path, then Flask can find other files easier
+app = Flask(__name__)
+
+
+# Route - mapping (connecting) a URL to Python function
+@app.route('/')
+def index():
+    return 'This is the home page.'
+
+
+# Runs app only when we run this script directly, not if we import this somewhere else
+if __name__ == "__main__":
+    app.run(debug=True)
